557b3d9ea1c1511b355031300e83ea7818e7ae79
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-05-19 12:24:35 +0700

    push

--- elearning/app/Http/Controllers/KelasController.php
+++ elearning/app/Http/Controllers/KelasController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Tugas extends Controller
+class KelasController extends Controller
 {
     //
 }
--- elearning/app/Http/Controllers/TugasController.php
+++ elearning/app/Http/Controllers/TugasController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Materi extends Controller
+class TugasController extends Controller
 {
     //
 }
